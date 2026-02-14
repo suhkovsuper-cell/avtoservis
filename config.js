f4d747282df725b7926551f8a12190f229687c69
suhkovsuper-cell <suhkovsuper@gmail.com>
2026-02-14 14:10:08 +0300

    config.js

--- config.js
+++ config.js
@@ -4,8 +4,14 @@ const JSONBIN_CONFIG = {
     MASTER_KEY: '$2a$10$IpgKewWGXPB9KuNg9Vl7m.ygKQOa5d4CpJ7KNnPpA3tqEZR372ivi'
 };
 
-// Конфигурация Telegram - ОТПРАВКА В ГРУППУ
+// Конфигурация Telegram
 const TELEGRAM_CONFIG = {
-    BOT_TOKEN: '8513239665:AAEEnuIzebYvxnHebKsjyrFnDpiVIn-R2VE',  // Токен бота
+    BOT_TOKEN: '8513239665:AAEEnuIzebYvxnHebKsjyrFnDpiVIn-R2VE',
     CHAT_ID: '966869280'  // ВАШ ЛИЧНЫЙ ID
+};
+
+// Конфигурация авторизации
+const AUTH_CONFIG = {
+    USERNAME: 'admin',
+    PASSWORD: 'admin'
 };
