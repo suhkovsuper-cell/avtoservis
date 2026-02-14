343e7bcc4440668e7fe901f4d41548d7681ec3a8
suhkovsuper-cell <suhkovsuper@gmail.com>
2026-02-14 13:01:43 +0300

    config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const JSONBIN_CONFIG = {
     MASTER_KEY: '$2a$10$IpgKewWGXPB9KuNg9Vl7m.ygKQOa5d4CpJ7KNnPpA3tqEZR372ivi'
 };
 
-// Конфигурация Telegram - НОВЫЙ БОТ @Serviquest36_bot
+// Конфигурация Telegram - отправка самому боту
 const TELEGRAM_CONFIG = {
-    BOT_TOKEN: '8513239665:AAEEnuIzebYvxnHebKsjyrFnDpiVIn-R2VE',  // Токен от @Serviquest36_bot
-    CHAT_ID: '-2031094030'  // ID группы (с минусом!)
+    BOT_TOKEN: '8513239665:AAEEnuIzebYvxnHebKsjyrFnDpiVIn-R2VE',
+    CHAT_ID: '8513239665'  // ID бота (для отправки самому себе)
 };
