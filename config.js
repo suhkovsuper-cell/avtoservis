8b2f4cd3a85f1f5b954e5974fb7966a191d59b43
suhkovsuper-cell <suhkovsuper@gmail.com>
2026-02-14 11:40:05 +0300

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 // Конфигурация JSONbin.io
-// Эти данные будут подставлены в основной код
 const JSONBIN_CONFIG = {
     BIN_ID: '6990220aae596e708f29d5c7',
     MASTER_KEY: '$2a$10$IpgKewWGXPB9KuNg9Vl7m.ygKQOa5d4CpJ7KNnPpA3tqEZR372ivi'
+};
+
+// Конфигурация Telegram
+const TELEGRAM_CONFIG = {
+    BOT_TOKEN: '6388621340:AAHwD1UVRPuor3AZUGZaQmCG8Q4-rNJtxeA',
+    CHAT_ID: '6388621340'  // Это ваш личный Chat ID
 };
